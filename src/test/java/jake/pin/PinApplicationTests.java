259ca3e8ec14c8ea1088e1399acbcd4b25f14862
jongyounlee <jayylee.dev@gmail.com>
2023-10-09 11:43:11 +0900

    start project

---
+++ src/test/java/jake/pin/PinApplicationTests.java
@@ -0,0 +1,13 @@
+package jake.pin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
