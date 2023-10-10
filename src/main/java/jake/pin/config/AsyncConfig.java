0da7fa7a15c5c3439bf7f739ceb78a160b39b2ce
jongyounlee <jayylee.dev@gmail.com>
2023-10-10 10:53:59 +0900

    add async config

---
+++ src/main/java/jake/pin/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package jake.pin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(3);
+        taskExecutor.setMaxPoolSize(100);
+        taskExecutor.setQueueCapacity(50);
+        taskExecutor.setThreadNamePrefix("Thread-");
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
